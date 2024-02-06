1a07203e250f0635705a83252c00214d00676aa9
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2024-02-06 22:19:28 +0500

    Add tests for the queen piece

--- spec/pieces/queen_spec.rb
+++ spec/pieces/queen_spec.rb
@@ -17,24 +17,21 @@ describe Chess::Queen do
 
     context 'when there is no piece expect the queen' do
       board = Chess::Board.from_fen('3q4/8/8/8/8/8/8/8 w - - 0 1')
-      board.display
       queen = board.board[0][3]
-      p board.board[0][2]
-      p board.board
       it 'returns many moves' do
         queen_moves = queen.valid_moves(board, [0, 3])
-        # correct_moves = []
-        p queen_moves
-        # expect(queen_moves).to eql correct_moves
+        correct_moves = [[1, 4], [2, 5], [3, 6], [4, 7], [1, 2], [2, 1], [3, 0], [1, 3], [2, 3], [3, 3], [4, 3],
+                         [5, 3], [6, 3], [7, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 2], [0, 1], [0, 0]]
+        expect(queen_moves).to eql correct_moves
       end
     end
 
     context 'when the queen has a the pawn in front of it moved 2 square' do
-      board = Chess::Board.from_fen('rnbqkbnr/pppppppp/8/8/P7/8/1PPPPPPP/RNBQKBNR b KQkq a3 0 1')
-      queen = board.board[7][0]
-      it 'returns many moves' do
-        queen_moves = queen.valid_moves(board, [7, 0])
-        correct_moves = [[6, 0], [5, 0]]
+      board = Chess::Board.from_fen('rnbqkbnr/ppp1pppp/8/3p4/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+      queen = board.board[0][3]
+      it 'returns correct moves' do
+        queen_moves = queen.valid_moves(board, [0, 3])
+        correct_moves = [[1, 3], [2, 3]]
         expect(queen_moves).to eql correct_moves
       end
     end
