a76aa035a1eb19769c2d1fd575b932a119975a15
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2024-04-09 18:12:53 +0500

    Comment a line in main.rb

--- lib/main.rb
+++ lib/main.rb
@@ -3,7 +3,7 @@
 require_relative 'chess_menu'
 
 chess = Chess::ChessMenu.new
-chess = Chess::Chess.from_fen('8/2k1r3/8/8/8/8/2K1R3/8 w - - 0 1')
+# chess = Chess::Chess.from_fen('8/2k1r3/8/8/8/8/2K1R3/8 w - - 0 1')
 
-# chess.display_menu
-chess.play
+chess.display_menu
+# chess.play
